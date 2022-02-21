5a7d0df7f5d79c6325646c15e1546d1ad85c7c2a
= <=>
2022-02-21 11:17:08 +0900

    연산자 수업

---
+++ JavaStudy_093/src/a03_연산자/Operation.java
@@ -0,0 +1,17 @@
+package a03_연산자;
+
+public class Operation {
+
+	public static void main(String[] args) {
+		//증감연산자
+		int score = 10;
+		//score = score + 1;
+		
+		//++(선증가)score
+		//score++(후증가->이다음에 증가)
+		System.out.println(score++);
+		System.out.println(score);//앞의 후증가에 의해서 더해짐(이미 증가되어있음)
+
+	}
+
+}
