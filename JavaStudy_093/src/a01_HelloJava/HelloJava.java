aa2c6b6186b480cca74b8650b039e617b2902159
= <=>
2022-02-21 11:13:56 +0900

    첫 수업 HelloJava 출력해보기

---
+++ JavaStudy_093/src/a01_HelloJava/HelloJava.java
@@ -0,0 +1,44 @@
+package a01_HelloJava;
+
+/**
+ * 
+ * 
+ * 여러줄 주석(클래스 또는 메소드를 설명하는 주석)
+ *
+ */
+
+/*
+
+ * 여러줄 주석(여러줄 메모)
+ */
+
+// 한줄 주석
+
+//주석의 단축키 Ctrl + /
+
+
+
+public class HelloJava {
+	
+	/** 
+	 * 
+	 * 프로그램의 시작 지점
+	 */
+
+	public static void main(String[] args) {
+		//'문자' - 한글자('ㅁ')
+		//"문자열" - 여러글자('ㅁㅁㅁㅁㅁ')
+		//프로그램 실행 ctrl + f11
+		System.out.println("hello java\n");
+		System.out.println("chaeeun\n");
+		
+		System.out.println("이름:\n 김채은");
+		System.out.println("연락처:\n 010-9988-1916");
+		System.out.println("주소:\n 부산 동래구 사직동");
+		
+		
+
+	}
+	
+
+}
